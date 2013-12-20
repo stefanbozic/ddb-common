683d2f75e00a1cb645208383c9e94bc22e8e5497
boz <stefan.bozic@fiz-karlsruhe.de>
2013-12-20 10:11:08 +0100

    - Fix problem with DI in nested services (remove @TestFor in tests) - add Copyright comments

--- grails-app/services/de/ddb/common/TestService.groovy
+++ grails-app/services/de/ddb/common/TestService.groovy
@@ -13,3 +13,15 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+package de.ddb.common
+
+
+class TestService {
+    def transactional = false
+
+
+
+    def test() {
+        return "test"
+    }
+}
--- test/integration/de/ddb/common/ConfigurationServiceTests.groovy
+++ test/integration/de/ddb/common/ConfigurationServiceTests.groovy
@@ -1,21 +1,35 @@
+/*
+ * Copyright (C) 2013 FIZ Karlsruhe
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package de.ddb.common
-
-import grails.test.mixin.TestFor
 
 import org.junit.Test
 
 /**
  * See the API for {@link grails.test.mixin.services.ServiceUnitTestMixin} for usage instructions
  */
-@TestFor(ConfigurationService)
 class ConfigurationServiceTests extends GroovyTestCase {
 
+    def configurationService
+
     @Test void getServiceTest() {
-        assert service != null
+        assert configurationService != null
     }
 
     @Test void getBackendTest() {
-        String echo = service.getBackendUrl()
+        String echo = configurationService.getBackendUrl()
 
         println echo
     }
